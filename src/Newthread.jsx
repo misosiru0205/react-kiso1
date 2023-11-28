22826888736538ce5048db23ee694b90e3ff0f44
misosiru0205 <misosiru0205@gmail.com>
2023-11-28 18:14:02 +0900

    git commit -u

--- src/Newthread.jsx
+++ src/Newthread.jsx
@@ -1,6 +1,9 @@
-import { useState } from "react"
+// import { useState } from "react"
 
 export const Newthread = () =>{
+
+    /*　使うか悩み…usestate無しでも大丈夫そう
+
         //Objectを格納するstateを定義
     const [obje,setObje] = useState({'title':""})
 
@@ -8,12 +11,13 @@ export const Newthread = () =>{
     function change(e){
         setObje({'title':(e.target.value)});
     }
+    */
 
         //送信クリック時の処理
     async function click(){
-        //console.log(obje)
+        const obje = {'title':(document.getElementById('newthread')).value}
         if(obje.title !== ""){ //入力値が空でない時に送信する
-            try{
+            try{ //例外処理
                     //fetchにてPOSTする　fetch("url",{method:"POST",~~~~~~~,~~~~~~})
                 const res = await fetch("https://railway.bulletinboard.techtrain.dev/threads",{
                     method:'POST',
@@ -23,7 +27,7 @@ export const Newthread = () =>{
                 console.log(res.json());
             }
             catch(error){
-                console.log(error); //エラー時の処理？
+                console.log(error); //エラー時の処理
             }
             
         }
@@ -31,14 +35,16 @@ export const Newthread = () =>{
     }
 
         //新規スレッド作成画面　input type="text"　で入力をし
-        //onChange={(e) => change(e)}にて即座にchange関数に入力値を渡す
+        //onChange={(e) => change(e)}にてchange関数に入力値を渡す
         //送信を押すことでonClickからclick関数に飛び入力した内容をAPIに送る
     return(
         <>
         <p>新規スレッド作成画面</p>
         <form>
-            <p>新規スレッド名<br/><input type="text" name="newthread" onChange={(e) => change(e)}></input>
-            <input type="button" value="送信" onClick={click}></input></p>
+            <label>新規スレッド名<br/>
+            <input type="text" id="newthread" /*onChange={(e) => change(e)}*/ required></input>
+            <input type="button" value="送信" onClick={click}></input>
+            </label>
         </form>
         <a href="http://localhost:3000/">掲示板TOP</a>
         </>
--- src/Threads.jsx
+++ src/Threads.jsx
@@ -1,17 +1,17 @@
 //useEffectとuseStateの呼び出し
-import React from "react";
 import { useEffect,useState } from "react"
 
 //スレッド取得用　コンポーネント
 export const Thread = () =>{
 
-    //useStateの定義
+        //useStateの定義
     const [thread,setThread] = useState([]);
 
-    //thread取得用関数　fetchを使うので asyncとawaitを使う
-        async function getthread(){
+        
+        //thread取得用関数　fetchを使うので asyncとawaitを使う
+    async function getthread(){
 
-            //APIにてスレッドを取得
+            //APIにてスレッドを取得 fetch("url",{method:"get"});
         const api = await fetch("https://railway.bulletinboard.techtrain.dev/threads",{method:'get'});
         const json = await api.json(); //json()でファイルの受け取り？
         const arr =[];//オブジェクトは.mapが使えない為、空の配列を用意
@@ -22,14 +22,14 @@ export const Thread = () =>{
         }
             //setThreadでthread内に配列arrを配置
         await setThread(arr);
-        }
+    }
 
         //ページを表示した際にスレッドを取得できるようにuseEffect内でgetThreadを使いthread内に
     useEffect(()=> {
         getthread();
     },[])
 
-
+        //thread内にはオブジェクトから変換した配列が入っているので.map()で取り出す
     return(
         <>
         <div className="list">
