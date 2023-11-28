79357a6c03a6e459af3fc8991fe2d3885888cc8c
misosiru0205 <misosiru0205@gmail.com>
2023-11-28 14:27:06 +0900

    git commit -u

--- src/Newthread.jsx
+++ src/Newthread.jsx
@@ -1,30 +1,38 @@
-import { useEffect,useState } from "react"
+import { useState } from "react"
 
 export const Newthread = () =>{
+        //Objectを格納するstateを定義
+    const [obje,setObje] = useState({'title':""})
 
-    const [obje,setObje] = useState({})
-
-
+        //inputから入力された値をstate内に保持する
     function change(e){
         setObje({'title':(e.target.value)});
     }
 
+        //送信クリック時の処理
     async function click(){
-        console.log(obje)
-        try{
-            const res = await fetch("https://railway.bulletinboard.techtrain.dev/threads",{
-                method:'POST',
-                headers:{'Content-Type':'application/json'},
-                body:JSON.stringify(obje)});
-
-            console.log(res.json());
-
+        //console.log(obje)
+        if(obje.title != ""){ //入力値が空でない時に送信する
+            try{
+                    //fetchにてPOSTする　fetch("url",{method:"POST",~~~~~~~,~~~~~~})
+                const res = await fetch("https://railway.bulletinboard.techtrain.dev/threads",{
+                    method:'POST',
+                    headers:{'Content-Type':'application/json'},
+                    body:JSON.stringify(obje)
+                });
+                console.log(res.json());
+            }
+            catch(error){
+                console.log(error); //エラー時の処理？
+            }
+            
         }
-        catch(error){
-            console.log(error);
-        }
+        else{}
     }
 
+        //新規スレッド作成画面　input type="text"　で入力をし
+        //onChange={(e) => change(e)}にて即座にchange関数に入力値を渡す
+        //送信を押すことでonClickからclick関数に飛び入力した内容をAPIに送る
     return(
         <>
         <p>新規スレッド作成</p>
